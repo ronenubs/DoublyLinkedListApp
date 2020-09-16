deab59013f3a717f808bf94eede652da9daab613
Ron Enubs <ronenubs@gmail.com>
2020-09-16 16:07:41 +0800

    revised

---
+++ DList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedListApp
+{
+    public class DList<E>
+    {
+        private DNode<E> header, trailer;
+        private int size;
+
+        public DList()
+        {
+            header = new DNode<E>(null, default, null);
+            trailer = new DNode<E>(header, default, null);
+            header.Next = trailer;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public bool IsEmpty()
+        {
+            return size <= 0;
+        }
+
+        public E First()
+        {
+            return header.Next.Element;
+        }
+
+        public E Last()
+        {
+            return trailer.Prev.Element;
+        }
+    }
+}
---
+++ DNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedListApp
+{
+    public class DNode<E>
+    {
+        private DNode<E> prev;
+        private E element;
+        private DNode<E> next;
+
+        public DNode(DNode<E> prev, E element, DNode<E> next)
+        {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+
+        public DNode<E> Prev
+        {
+            get { return prev; }
+            set { prev = value; }
+        }
+
+        public E Element
+        {
+            get { return element; }
+            set { element = value; }
+        }
+
+        public DNode<E> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}
